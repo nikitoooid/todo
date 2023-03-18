a32921c299f8c35d612fb99f99a37f6a58aee847
nikitoooid <nekytnp@gmail.com>
2023-03-19 00:03:44 +0200

    User can see all his tasks

---
+++ spec/features/tasks/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'User can see all his tasks', "
+  In order to remember what I want to do
+  As an authenticated user
+  I'd like to be able to see all my tasks
+" do
+  let(:user) { create(:user) }
+  let!(:tasks) { create_list(:task, 3, user: user) }
+
+  it 'Registred user see all his tasks' do
+    sign_in(user)
+    visit tasks_path
+
+    tasks.each do |task|
+    	expect(page).to have_content(task.title)
+    end
+  end
+
+  it 'Unregistred user can not see any tasks' do
+    visit tasks_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -5,7 +5,31 @@ RSpec.describe "Tasks", type: :request do
   let(:task) { create(:task, user: user) }
 
   describe "GET /index" do
-    
+    let!(:tasks) { create_list(:task, 3, user: user) }
+
+    context "when user is authenticated" do
+      before do
+        sign_in user
+        get tasks_path
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "displays user's tasks" do
+        tasks.each do |task|
+          expect(response.body).to include(task.title)
+        end
+      end
+    end
+
+    context "when user is not authenticated" do
+      it "redirects to sign in page" do
+        get tasks_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
 
   describe "GET /show" do
@@ -18,6 +42,10 @@ RSpec.describe "Tasks", type: :request do
       get '/tasks/new'
     end
 
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
     it 'renders new veiw' do
       expect(response).to render_template :new
     end
@@ -26,27 +54,51 @@ RSpec.describe "Tasks", type: :request do
   describe "POST /create" do
     before { sign_in user }
 
-    context 'with valid attributes' do
-      it 'saves a new task to database' do
+    context "with valid attributes" do
+      let(:task_attributes) { attributes_for(:task) }
+
+      it "creates a new task for the user" do
         expect { post '/tasks', params: { task: attributes_for(:task) } }.to change(Task, :count).by(1)
       end
 
-      it 'redirects to index view' do
-        post '/tasks', params: { task: attributes_for(:task) }
-        expect(response).to redirect_to tasks_path
+      it "redirects to the tasks path" do
+        post tasks_path, params: { task: task_attributes }
+        expect(response).to redirect_to(tasks_path)
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not save the task' do
-        expect { post '/tasks', params: { task: attributes_for(:task, :invalid) } }.not_to change(Task, :count)
+    context "with invalid attributes" do
+      it "does not create a new task for the user" do
+        expect { post tasks_path, params: { task: attributes_for(:task, :invalid) } }.not_to change(Task, :count)
       end
 
-      it 're-renders new view' do
-        post '/tasks', params: { task: attributes_for(:task, :invalid) }
-        expect(response).to render_template :new
+      it "renders the new template" do
+        post tasks_path, params: { task: attributes_for(:task, :invalid) }
+        expect(response).to render_template(:new)
       end
     end
+
+    # context 'with valid attributes' do
+    #   it 'saves a new task to database' do
+    #     expect { post '/tasks', params: { task: attributes_for(:task) } }.to change(Task, :count).by(1)
+    #   end
+
+    #   it 'redirects to index view' do
+    #     post '/tasks', params: { task: attributes_for(:task) }
+    #     expect(response).to redirect_to tasks_path
+    #   end
+    # end
+
+    # context 'with invalid attributes' do
+    #   it 'does not save the task' do
+    #     expect { post '/tasks', params: { task: attributes_for(:task, :invalid) } }.not_to change(Task, :count)
+    #   end
+
+    #   it 're-renders new view' do
+    #     post '/tasks', params: { task: attributes_for(:task, :invalid) }
+    #     expect(response).to render_template :new
+    #   end
+    # end
   end
 
   describe "PATCH /update" do
