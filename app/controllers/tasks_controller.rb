87123108f53ee6f999f11a5bdcc8a9ea7dbd1083
nikitoooid <nekytnp@gmail.com>
2023-03-28 17:19:59 +0300

    Rubocoped

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,11 @@ class TasksController < ApplicationController
     @tasks = current_user.tasks
   end
 
+  def show; end
+
   def new
     @task = Task.new
   end
-
-  def show; end
 
   def edit; end
 
